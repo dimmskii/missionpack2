f9abd6189964c531744a4720dbf5c80d89d8ace7
Dmitri <admin@quaker.pro>
2026-07-24 13:55:58 -0400

    Revert "Register team HUD media unconditionally, not gated on gametype"
    This reverts commit 130356447af2651205b63e08d1de16eb083cb193.

--- src/code/cgame/cg_main.c
+++ src/code/cgame/cg_main.c
@@ -845,8 +845,7 @@ static void CG_RegisterGraphics( void ) {
 //#endif
 
 //	if ( cgs.gametype >= GT_TEAM || cg_buildScript.integer ) {
-//	if ( GT_IsTeam(cgs.gametype) || cg_buildScript.integer ) { // ~Dimmskii
-	{ // ~Dimmskii
+	if ( GT_IsTeam(cgs.gametype) || cg_buildScript.integer ) { // ~Dimmskii
 		cgs.media.friendShader = trap_R_RegisterShader( "sprites/foe" );
 		cgs.media.redQuadShader = trap_R_RegisterShader("powerups/blueflag" );
 		cgs.media.teamStatusBar = trap_R_RegisterShader( "gfx/2d/colorbar.tga" );
