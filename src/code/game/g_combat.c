c59823dcb2691aa4f72b3b2b723742dd1294e08b
Dmitri <admin@quaker.pro>
2026-08-10 19:41:26 -0400

    Revert scoring changes regarding round-based/arena types

--- src/code/game/g_combat.c
+++ src/code/game/g_combat.c
@@ -526,14 +526,14 @@ void player_die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 		if ( attacker == self || OnSameTeam (self, attacker ) ) {
 //			AddScore( attacker, self->r.currentOrigin, -1 );
 // ~Dimmskii
-			if ( !GT_IsRoundBased(g_gametype.integer) ) {  // Suicides don't subtract points in arena or freeze tag gamemodes.
+			if ( !GT_IsArenaGame(g_gametype.integer) ) {  // Suicides don't subtract points in arena gamemodes.
 				AddScore( attacker, self->r.currentOrigin, -1 );
 			}
 // END Dimmskii
 		} else {
 //			AddScore( attacker, self->r.currentOrigin, 1 );
 // ~Dimmskii
-			if ( GT_IsRoundBased(g_gametype.integer) ) {
+			if ( GT_IsArenaGame(g_gametype.integer) ) {
 				AddScore( attacker, self->r.currentOrigin, 100 );
 			} else {
 				AddScore( attacker, self->r.currentOrigin, 1 );
@@ -572,7 +572,7 @@ void player_die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
 		//AddScore( self, self->r.currentOrigin, -1 );
 
 		// ~Dimmskii
-		if ( !GT_IsRoundBased(g_gametype.integer) ) {  // Suicides don't subtract points in arena or freeze tag gamemodes.
+		if ( !GT_IsArenaGame(g_gametype.integer) ) {  // Suicides don't subtract points in arena gamemodes.
 			AddScore( self, self->r.currentOrigin, -1 );
 		}
 		// END Dimmskii
